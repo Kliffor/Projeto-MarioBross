2855a76a5fafb9c6abc03b2b656d558ebb1b2198
kliffor <kliffor@gmail.com>
2025-04-10 18:54:16 -0300

    10042025-18:54

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 
 const form = document.querySelector(".formulario-fale-conosco")
+let mascara = document.querySelector(".mascara-formulario")
 
 function cliqueiNoBotao(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+  
+}
+  
+function clicarNaMascara(){
+    mascara.style.visibility = "hidden"
+    form.style.left = "-110px"
+    form.style.top = "30%"   
 }
